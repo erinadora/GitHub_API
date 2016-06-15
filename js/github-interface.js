b24ed1c5f9a13dacd17e64d1036fd70c5f1832da
Rachael Bermudes <bermudesorama@gmail.com>
2016-06-15 14:53:32 -0700

    add clear output and repos, refactored pass username from front to backend

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,10 +2,10 @@ var getRepos = require('./../js/github.js').getRepos;
 
 $(document).ready(function(){
   $('#github').submit(function(event){
+    $('#output,#repos').empty();
     event.preventDefault();
     var username = $('#username').val();
-    getRepos();
+    getRepos(username);
     $('input#username').val('');
-    $('#results').val('');
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,7 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
-  var github = $('#username').val();
+exports.getRepos = function(github){
   $.get('https://api.github.com/users/' + github + '/repos?access_token=' + apiKey + '&per_page=500&sort=update').then(function(response){
     console.log(response);
     $('#output').append('<h2>' + response[0].owner.login + '</h2>');
@@ -11,7 +10,7 @@ exports.getRepos = function(){
       $('#repos').append('<br><li><img id="arrow" src="./../images/arrow.png"> &nbsp' + response[i].full_name +  response[i].description + '<a class="view" target="_blank" href="' + response[i].html_url + '">&nbsp &nbsp View</a></li>');
     }
   }).fail(function(error){
-  console.log(error.responseJSON.message);
-  $('.repos').append('<h2>No repositories found</h2>');
-});
+    console.log(error.responseJSON.message);
+    $('.repos').append('<h2>No repositories found</h2>');
+  });
 };
